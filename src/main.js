6be7d79051dbdfb2483595db5c8a9be057c1aa2d
MaxPress <maxpress96@ukr.net>
2025-04-24 23:32:18 +0300

    add html, css, js and js modules, img and svg

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+
+import Swiper from 'swiper';
+import 'swiper/css';
+
+import * as themeSwitcher from './js/theme-switcher';
