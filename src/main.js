1f9cfcc97a79c58c28bd84d05470e82edf75b46d
MaxPress <maxpress96@ukr.net>
2025-04-26 21:45:59 +0300

    add icons axios and izitoast

--- src/main.js
+++ src/main.js
@@ -4,4 +4,9 @@ import "accordion-js/dist/accordion.min.css";
 import Swiper from 'swiper';
 import 'swiper/css';
 
+import iziToast from "izitoast";
+import 'izitoast/dist/css/iziToast.min.css';
+
+import axios from 'axios';
+
 import * as themeSwitcher from './js/theme-switcher';
